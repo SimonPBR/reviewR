722a8d926abe166ad1f2ac3448adedaca7cccc7f
Simon Pabst <s.pabst@student.rug.nl>
2019-12-10 15:44:34 +0100

    Added label to plot

--- R/Plot.R
+++ R/Plot.R
@@ -8,9 +8,9 @@ plotMovie <- function(queryTitle, color = "green"){
       geom_point(alpha = 0.4)  +
       labs(title = queryTitle) +
       geom_point(data = movies[movies$title == queryTitle,],
-             color = color) #+
-      # geom_label_repel(aes(label = mName),
-      #                  data = movies[movies$title == mName,],
-      #                  box.padding   = 0.5,
-      #                  label.padding = 0.5)
+             color = color) +
+      geom_label_repel(aes(label = queryTitle),
+                       data = movies[movies$title == queryTitle,],
+                       box.padding   = 0.5,
+                       label.padding = 0.5)
 }
